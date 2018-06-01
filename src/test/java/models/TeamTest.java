1b2206ce894d2f7d2a849a16d5bd148249b47a67
Student <example@example.com>
2018-06-01 09:14:02 -0700

    Add constructor to Team instance test

--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -1,12 +1,13 @@
 package models;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TeamTest {
 
     @Test
     public void team_instantiatesNewInstanceCorrectly_true() throws Exception {
-        Team testTeam = new Team();
-
+        Team testTeam = new Team("Blue", "They sure aren't Red", "Jon Jonson");
+        assertEquals(true, testTeam instanceof Team);
     }
 }
