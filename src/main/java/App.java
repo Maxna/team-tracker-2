611b25bc68ff06998c105ed586284389fd02a81b
Student <example@example.com>
2018-06-01 15:08:11 -0700

    Reorder routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -21,16 +21,8 @@ public class App {
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             ArrayList<Team> teams = Team.getAll();
-            model.put("team", teams);
+            model.put("teams", teams);
             return new ModelAndView(model, "index.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        get("/teams/:id", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            int idOfTeamToFind = Integer.parseInt(req.params("id"));
-            Team foundTeam = Team.findById(idOfTeamToFind);
-            model.put("team", foundTeam);
-            return new ModelAndView(model, "team-detail.hbs");
         }, new HandlebarsTemplateEngine());
 
         post("/teams/new", (req, res) -> {
@@ -43,7 +35,15 @@ public class App {
             return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
 
-        get("/jobs/:id/update", (req, res) -> {
+        get("/teams/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfTeamToFind = Integer.parseInt(req.params("id"));
+            Team foundTeam = Team.findById(idOfTeamToFind);
+            model.put("team", foundTeam);
+            return new ModelAndView(model, "team-detail.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/teams/:id/update", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             int idOfTeamToEdit = Integer.parseInt(req.params("id"));
             Team editTeam = Team.findById(idOfTeamToEdit);
@@ -51,15 +51,13 @@ public class App {
             return new ModelAndView(model, "team-form.hbs");
         }, new HandlebarsTemplateEngine());
 
-        post("/jobs/:id/update", (req, res) -> {
+        post("/teams/:id/update", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             String newTeamName = req.queryParams("teamName");
-            String newDescription = req.queryParams("description");
-            String newMember = req.queryParams("member");
-            int idOfJobToEdit = Integer.parseInt(req.params("id"));
-            Team editTeam = Team.findById(idOfJobToEdit);
+            int idOfTeamToEdit = Integer.parseInt(req.params("id"));
+            Team editTeam = Team.findById(idOfTeamToEdit);
             editTeam.updateTeam(newTeamName);
-            return new ModelAndView(model, "success/hbs");
+            return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
 
     }
