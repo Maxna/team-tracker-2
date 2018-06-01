146015264cc9f5ada91b09aaabb252a2a804331a
Student <example@example.com>
2018-06-01 11:55:40 -0700

    Add success.hbs route to post new team

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,6 +13,35 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Team> teams = Team.getAll();
+            model.put("team", teams);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/teams/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfTeamToFind = Integer.parseInt(req.params("id"));
+            Team foundTeam = Team.findById(idOfTeamToFind);
+            model.put("team", foundTeam);
+            return new ModelAndView(model, "team-detail.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/teams/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "team-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/teams/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String teamName = req.queryParams("teamName");
+            String description = req.queryParams("description");
+            String member = req.queryParams("member");
+            Team newTeam = new Team(teamName, description, member);
+            model.put("team", newTeam);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
 
     }
 }
