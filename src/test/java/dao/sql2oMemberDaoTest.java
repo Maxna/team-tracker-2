ec3a854a80364887cd509c5b2028f152e30ae972
Matt Nardoni <mattnardoni@MattNardoni.local>
2018-06-08 15:41:22 -0700

    Add member and team sql2oDaoTest files

---
+++ src/test/java/dao/sql2oMemberDaoTest.java
@@ -0,0 +1,24 @@
+package dao;
+
+import models.Member;
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class sql2oMemberDaoTest {
+    private sql2oMemberDao memberDao;
+    private Connection conn;
+
+    @Before
+    public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        memberDao = new Sql2oMemberDao(sql2o);
+        conn = sql2o.open();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        conn.close();
+    }
+}
